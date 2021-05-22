d9e7a983f14303a51aa817126ec2a5d9e5401b55
Michael Martinez <zeroeclipse@live.com>
2021-05-22 19:06:12 -0400

    commit tree 405d74d74603e37a5f79dc92bcdebaa11be5ee9f

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+
+//difference between function and function expression?
+function saturdayFun(string="roller-skate") {//function
+    return "This Saturday, I want to " + string + "!"
+}
+
+let mondayWork = function(string="go to the office"){//function expression
+    return "This Monday, I will " + string + "."
+}
+
+let wrapAdjective = function(param="*") {
+    return function(adjective="special") {
+      return `You are ${param}${adjective}${param}!`
+    }
+  }
+// difference between let and const
+let Calculator = {
+    add(a, b) {
+        return a + b
+    },
+    subtract(a, b) {
+        return a - b
+    },
+    multiply(a, b) {
+        return a * b
+    },
+    divide(a, b) {
+        return a / b
+    }
+}
+function actionApplyer(int,array) {
+    for (let i = 0; i < array.length; i++ ){
+        int = array[i](int)
+      }
+    
+      return int
+}
